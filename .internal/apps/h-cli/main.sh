93f7e1a7349b709f3c1ef798f2e95ab8d61a89af
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-02-18 13:59:12 +0530

    rev

--- .internal/apps/h-cli/main.sh
+++ .internal/apps/h-cli/main.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-h_cli_main2() {
-  tac ~/.bash_history | \
-  fzf --reverse --height 50% | \
-  xargs -I {} bash -c "{}"
-}
-
 h_cli_main() {
   cat ~/.bash_history | \
   nl | \
   tac | \
-  fzf --nth=2 --prompt "search commands> " | \
+  fzf --height 1 \
+  --prompt "search commands> " | \
   awk '{ $1=""; print substr($0,2) }' | \
+  # xargs -I {} echo "<{}>"
   xargs -I {} bash -c "{}"
 }
