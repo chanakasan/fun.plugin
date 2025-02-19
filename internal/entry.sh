6a68274143a66ee298115dec56b69bc4c4df24b1
Chanaka Abeysinghe <chanakasan@gmail.com>
2025-02-19 16:05:46 +0530

    rev

--- internal/entry.sh
+++ internal/entry.sh
@@ -1,8 +1,8 @@
 nx_fun_root="$HOME/dotfiles/paks/fun-cli.pak"
 
-export PATH=$PATH:$nx_fun_root/bin
+export PATH=$nx_fun_root/bin:$PATH
 
-for f in $nx_fun_root/src/entrypoints/*.sh; do
+for f in $nx_fun_root/internal/entrypoints/*.sh; do
   if [ -f "$f" ]; then
       source "$f"
   fi
